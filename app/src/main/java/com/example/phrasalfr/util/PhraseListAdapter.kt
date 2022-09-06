11d28c225d8e13fde19cd6d92e6584e94ae9faf4
cawoolf <rayadevinfo@gmail.com>
2022-09-05 21:09:52 -0400

    starting edit DB

--- app/src/main/java/com/example/phrasalfr/util/PhraseListAdapter.kt
+++ app/src/main/java/com/example/phrasalfr/util/PhraseListAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.phrasalfr.util
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
-class PhraseListAdapter {
+
+class PhraseListAdapter() : RecyclerView.Adapter<PhraseListAdapter.PhraseViewHolder>() {
+
+    class PhraseViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhraseViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PhraseViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
