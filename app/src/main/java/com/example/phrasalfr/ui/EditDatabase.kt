3186e661ed584d218e2072b8b50bcbcd24bf9314
cawoolf <rayadevinfo@gmail.com>
2022-09-05 21:46:09 -0400

    adpater progress

--- app/src/main/java/com/example/phrasalfr/ui/EditDatabase.kt
+++ app/src/main/java/com/example/phrasalfr/ui/EditDatabase.kt
@@ -1,12 +1,39 @@
 package com.example.phrasalfr.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.phrasalfr.R
+import com.example.phrasalfr.databinding.ActivityEditDatabaseBinding
+import com.example.phrasalfr.databinding.ActivityMainBinding
+import com.example.phrasalfr.util.PhraseListAdapter
 
 class EditDatabase : AppCompatActivity() {
+
+   private lateinit var recyclerView : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_database)
+
+        val binding = ActivityEditDatabaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.editDbRecyclerView
+
+        loadDataIntoRecyclerView()
     }
+
+    private fun loadDataIntoRecyclerView() {
+
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.adapter = PhraseListAdapter()
+    }
+
+
 }
--- app/src/main/java/com/example/phrasalfr/util/PhraseListAdapter.kt
+++ app/src/main/java/com/example/phrasalfr/util/PhraseListAdapter.kt
@@ -1,21 +1,45 @@
 package com.example.phrasalfr.util
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.phrasalfr.R
+import com.example.phrasalfr.database.Phrase
 
 
-class PhraseListAdapter() : RecyclerView.Adapter<PhraseListAdapter.PhraseViewHolder>() {
+class PhraseListAdapter(private val allPhrases: List<Phrase>) : RecyclerView.Adapter<PhraseListAdapter.PhraseViewHolder>() {
+
+    private val phrases = allPhrases
 
     class PhraseViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        var category = view.findViewById<TextView>(R.id.list_item_category)
+        val english = view.findViewById<TextView>(R.id.list_item_english)
+        val french = view.findViewById<TextView>(R.id.list_item_french)
+        val delete = view.findViewById<Button>(R.id.list_item_delete)
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhraseViewHolder {
-        TODO("Not yet implemented")
+        val layout = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.phrase_list_item, parent, false)
+
+        return PhraseViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: PhraseViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = phrases[position]
+
+        holder.category.text = item.category
+        holder.english.text = item.phraseEnglish
+        holder.french.text = item.phraseFrench
+        holder.delete.setOnClickListener {
+            //deleteEntry()
+        }
+
     }
 
     override fun getItemCount(): Int {
